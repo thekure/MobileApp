f2dcdda3b84d521d6d004861ef575634dea31160
Laurits <laku@itu.dk>
2024-02-06 16:08:41 +0100

    exercise 2, without documentation

--- Lecture01/CphBuzz/app/src/main/java/dk/itu/moapd/copenhagenbuzz/laku/Event.kt
+++ Lecture01/CphBuzz/app/src/main/java/dk/itu/moapd/copenhagenbuzz/laku/Event.kt
@@ -1,54 +1,17 @@
 package dk.itu.moapd.copenhagenbuzz.laku
 
-class Event(
-    private var eventName: String,
-    private var eventLocation: String,
-    private var eventDate: String,
-    private var eventType: String,
-    private var eventDescription: String
+enum class EventType {
+    BIRTHDAY,
+    WEDDING,
+    CONFERENCE
+}
+data class Event(
+    var eventName: String,
+    var eventLocation: String,
+    var eventDate: String,
+    var eventType: EventType,
+    var eventDescription: String
 ) {
-
-
-    fun getEventName(): String {
-        return eventName
-    }
-
-    fun setEventName(eventName: String) {
-        this.eventName = eventName
-    }
-
-    fun getEventLocation(): String {
-        return eventLocation
-    }
-
-    fun setEventLocation(eventLocation: String) {
-        this.eventLocation = eventLocation
-    }
-
-    fun getEventDate(): String {
-        return eventDate
-    }
-
-    fun setEventDate(eventDate: String) {
-        this.eventDate = eventDate
-    }
-
-    fun getEventType(): String {
-        return eventType
-    }
-
-    fun setEventType(eventType: String) {
-        this.eventType = eventType
-    }
-
-    fun getEventDescription(): String {
-        return eventDescription
-    }
-
-    fun setEventDescription(eventDescription: String) {
-        this.eventDescription = eventDescription
-    }
-    //  Event ( eventName = ’Something’,eventLocation = ’Copenhagen’)eventDate = ’Tue, Jan 02 2024 - Wed, Jan 10 2024’)eventType = ’Wedding’)eventDescription = ’Something more.’)
     override fun toString(): String {
         return "Event (eventName = ’$eventName’, " +
                 "eventLocation = ’$eventLocation’) " +
--- Lecture01/CphBuzz/app/src/main/java/dk/itu/moapd/copenhagenbuzz/laku/MainActivity.kt
+++ Lecture01/CphBuzz/app/src/main/java/dk/itu/moapd/copenhagenbuzz/laku/MainActivity.kt
@@ -2,13 +2,17 @@ package dk.itu.moapd.copenhagenbuzz.laku
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.EditText
+import android.widget.Toast
 import androidx.core.view.WindowCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.snackbar.Snackbar
 import dk.itu.moapd.copenhagenbuzz.laku.databinding.ActivityMainBinding
 import java.util.Date
 import java.text.SimpleDateFormat
@@ -23,16 +27,8 @@ class MainActivity : AppCompatActivity() {
         private val TAG = MainActivity::class.qualifiedName
     }
 
-    // GUI variables
-    private lateinit var eventName: EditText
-    private lateinit var eventLocation: EditText
-    private lateinit var eventDate: EditText
-    private lateinit var eventType: AutoCompleteTextView
-    private lateinit var eventDescription: EditText
-    private lateinit var addEventButton: FloatingActionButton
-
     // An instance of the 'Event' class.
-    private val event: Event = Event("","", "", "", "")
+    private lateinit var event: Event
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window,false)
@@ -41,71 +37,93 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Link the UI components with the Kotlin source-code
-        eventName        = findViewById(R.id.edit_text_event_name)
-        eventLocation    = findViewById(R.id.edit_text_event_location)
-        eventDate        = findViewById(R.id.edit_text_event_date)
-        eventType        = findViewById(R.id.auto_complete_event_types)
-        eventDescription = findViewById(R.id.edit_text_event_description)
-        addEventButton   = findViewById(R.id.fab_add_event)
-
-
-        val eventTypes = resources.getStringArray(R.array.event_types)
-        val arrayAdapter = ArrayAdapter(this, R.layout.event_type_dropdown, eventTypes)
-        eventType.setAdapter(arrayAdapter)
-
-        eventDate.setOnClickListener {
-            val datePicker =
-                MaterialDatePicker.Builder.dateRangePicker()
-                    .setTitleText("Select dates")
-                    .build()
-
-            datePicker.show(supportFragmentManager, "tag")
-
-            datePicker.addOnPositiveButtonClickListener { selection ->
-                val dateFormat = SimpleDateFormat("EEE, MMM dd yyyy", Locale.ENGLISH)
-                val startDate = dateFormat.format(Date(selection.first))
-                val endDate = dateFormat.format(Date(selection.second))
-                val combinedString = "$startDate - $endDate"
-                eventDate.setText(combinedString)
-            }
-        }
-
-        // Listener for user interaction in the 'Add Event' button
-        addEventButton.setOnClickListener {
-            // Only execute the following code when the user fills all 'EditText'
-            if(eventName.text.toString().isNotEmpty() &&
-                eventLocation.text.toString().isNotEmpty()) {
-
-                // Update the object attributes
-                event.setEventName(
-                    eventName.text.toString().trim()
-                )
-                event.setEventLocation(
-                    eventLocation.text.toString().trim()
-                )
-                event.setEventDate(
-                    eventDate.text.toString().trim()
-                )
-
-                event.setEventType(
-                    eventType.text.toString().trim()
-                )
-
-                event.setEventDescription(
-                    eventDescription.text.toString().trim()
-                )
-
-                // Write in the 'Logcat' system
-                showMessage()
-            }
-        }
-
+        eventTypeSetup()
+        setListeners()
         setContentView(binding.root)
     }
 
-    private fun showMessage() {
-        Log.d(TAG, event.toString())
+    private fun setListeners() {
+        with(binding.contentMain) {
+            editTextEventDate.setOnFocusChangeListener { _, hasFocus ->
+                if (hasFocus){
+                    handleDatePickerAction()
+                }
+            }
+
+            fabAddEvent.setOnClickListener {
+                handleAddEventAction(it)
+            }
+        }
+    }
+
+    private fun handleDatePickerAction(){
+        val datePicker =
+            MaterialDatePicker.Builder.dateRangePicker()
+                .setTitleText("Select dates")
+                .build()
+
+        datePicker.show(supportFragmentManager, "tag")
+
+        datePicker.addOnPositiveButtonClickListener { selection ->
+            val dateFormat = SimpleDateFormat("EEE, MMM dd yyyy", Locale.ENGLISH)
+            val startDate = dateFormat.format(Date(selection.first))
+            val endDate = dateFormat.format(Date(selection.second))
+            val combinedString = "$startDate - $endDate"
+            with(binding.contentMain.editTextEventDate){
+                if(startDate == endDate){
+                    setText(startDate)
+                } else {
+                    setText(combinedString)
+                }
+                clearFocus()
+            }
+        }
+    }
+
+    private fun eventTypeSetup(){
+        val eventTypes = resources.getStringArray(R.array.event_types)
+        val arrayAdapter = ArrayAdapter(this, R.layout.event_type_dropdown, eventTypes)
+
+        binding.contentMain.autoCompleteEventTypes.setAdapter(arrayAdapter)
+    }
+
+    private fun handleAddEventAction(view: View){
+        with(binding.contentMain) {
+            // Only execute the following code when the user fills all 'EditText'
+            if (checkInputValidity()) {
+                event = Event(
+                    editTextEventName.text.toString().trim(),
+                    editTextEventLocation.text.toString().trim(),
+                    editTextEventDate.text.toString().trim(),
+                    enumValueOf(autoCompleteEventTypes.text.toString().uppercase()),
+                    editTextEventDescription.text.toString().trim()
+                )
+
+                // Write in the 'Logcat' system
+                showMessage(view)
+                hideKeyboard()
+            } else {
+                Toast.makeText(this@MainActivity, "You need to fill out all fields first.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun showMessage(view: View) {
+        Snackbar.make(view, event.toString(), Snackbar.LENGTH_SHORT).show()
+    }
+
+    private fun checkInputValidity(): Boolean =
+        with(binding.contentMain){
+                editTextEventName.text.toString().isNotEmpty() &&
+                editTextEventLocation.text.toString().isNotEmpty() &&
+                editTextEventDate.text.toString().isNotEmpty() &&
+                autoCompleteEventTypes.text.toString().isNotEmpty() &&
+                editTextEventDescription.text.toString().isNotEmpty()
+        }
+
+    private fun hideKeyboard(){
+        val imm = getSystemService(InputMethodManager::class.java)
+        imm?.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
     }
 }
 
